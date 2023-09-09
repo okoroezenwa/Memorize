d78f4965dc1af1cb2eea4931b3d7ba4313c6b063
Ezenwa Okoro <ezenwa.okoro@ibm.com>
2023-09-09 15:25:23 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ezenwa Okoro on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
